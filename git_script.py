e7736afada5d052203fa1358ad3a39b5cbeb6aa5
邓润庭 <1032939141@qq.com>
2018-11-27 15:28:19 +0800

    test

--- git_script.py
+++ git_script.py
@@ -1,7 +1,38 @@
 # coding=utf-8
 # 自动提交合并代码
+import os
+
+
+class gitScript(object):
+    def __init__(self):
+        self.current_branch = self.getCurBranchName()
+
+    def run(self, commit):
+        os.system("git commit -am '{}'".format(commit))
+        os.system("git push origin {}".format(self.current_branch))
+        os.system("git checkout develop")
+
+        os.system("git pull origin develop")
+
+        os.system("git merge {}".format(self.current_branch))
+
+        os.system(":")
+        os.system("q\n")
+
+        os.system("git push origin develop")
+        os.system("git checkout {}".self.current_branch)
+
+
+    def getCurBranchName(self):
+        for line in os.popen("git branch"):
+            if line.startswith("*"):
+                return line[1:]
 
 
 
 
 
+if __name__ == "__main__":
+    g = gitScript()
+    g.run("test")
+    print(g.current_branch)
